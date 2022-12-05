fbf9013ec62863997e1a12b74ab0f8859490314f
David Peloso <david.g.peloso@gmail.com>
2022-12-04 20:31:53 -0500

    Install Semantic ui

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,6 @@
 # Pin npm packages by running ./bin/importmap
+# https://code.jquery.com/jquery-3.6.0.min.js
 
-pin "application", preload: true
+pin 'application', preload: true
+pin 'jquery', to: 'https://ga.jspm.io/npm:jquery@3.6.1/dist/jquery.js'
+pin 'semantic-ui', to: 'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js'
